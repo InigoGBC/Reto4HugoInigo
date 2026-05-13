31043347ad35bf39f2fe0a7bf877fb91f9d4e282
HugoMoura88 <1DAM@A11-PC16>
2026-05-13 13:50:15 +0200

    ej

--- Eva3RetoHugoInigo/src/app/Main2.java
+++ Eva3RetoHugoInigo/src/app/Main2.java
@@ -1,5 +1,8 @@
 package app;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 import dao.FacturaDAO;
 import dao.LineaFacturaDAO;
 import modelo.LineaFactura;
@@ -7,12 +10,16 @@ import modelo.LineaFactura;
 public class Main2 {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in).useLocale(Locale.US);
 		// Muestra una factura por id junto con todas sus líneas de factura. HUGO ***
 		FacturaDAO facturaDAO = new FacturaDAO();
 		LineaFacturaDAO lineafacturaDAO = new LineaFacturaDAO();
-		System.out.println(facturaDAO.obtenerPorId(1));
+		System.out.println("Ej1\nEscribe la id de una factura: ");
+		String idej1st = sc.nextLine();
+		int idej1 = Integer.parseInt(idej1st);
+		System.out.println(facturaDAO.obtenerPorId(idej1));
 		for (LineaFactura lf : lineafacturaDAO.obtenerTodos()) {
-			if (lf.getIdFactura() == 1)
+			if (lf.getIdFactura() == idej1)
 			System.out.println(lf);
 		}
 		
