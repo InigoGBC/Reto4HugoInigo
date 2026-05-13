ac0cc0b04850c5f37829d2b1aa69349e0be3e7bd
HugoMoura88 <1DAM@A11-PC16>
2026-05-13 13:23:20 +0200

    asd

--- Eva3RetoHugoInigo/src/app/Main2.java
+++ Eva3RetoHugoInigo/src/app/Main2.java
@@ -1,10 +1,13 @@
 package app;
 
+import dao.FacturaDAO;
+
 public class Main2 {
 
 	public static void main(String[] args) {
 		// Muestra una factura por id junto con todas sus líneas de factura. HUGO ***
-		
+		FacturaDAO facturaDAO = new FacturaDAO();
+		System.out.println(facturaDAO.obtenerPorId(1));
 		
 		
 		
