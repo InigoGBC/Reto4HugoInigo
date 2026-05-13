b27f5326a433fb86177868a006127116fce95981
HugoMoura88 <1DAM@A11-PC16>
2026-05-13 13:57:26 +0200

    as

--- Eva3RetoHugoInigo/src/app/Main2.java
+++ Eva3RetoHugoInigo/src/app/Main2.java
@@ -11,10 +11,12 @@ public class Main2 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in).useLocale(Locale.US);
-		// Muestra una factura por id junto con todas sus líneas de factura. HUGO ***
+		
 		FacturaDAO facturaDAO = new FacturaDAO();
 		LineaFacturaDAO lineafacturaDAO = new LineaFacturaDAO();
-		System.out.println("Ej1\nEscribe la id de una factura: ");
+		
+		// Ej 3 Muestra una factura por id junto con todas sus líneas de factura. 
+		System.out.println("Ej3\nEscribe la id de una factura: ");
 		String idej1st = sc.nextLine();
 		int idej1 = Integer.parseInt(idej1st);
 		System.out.println(facturaDAO.obtenerPorId(idej1));
@@ -22,8 +24,16 @@ public class Main2 {
 			if (lf.getIdFactura() == idej1)
 			System.out.println(lf);
 		}
+		//Fin Ej 3
+		//-----------------------------------------------------------------------
+		/* Ej 4 Muestra todas las facturas de un mes, pide el número del mes y 
+		   calcula el total facturado ese mes. */
 		
 		
+		
+		
+		
+		//Fin Ej 4
 		
 		
 		
